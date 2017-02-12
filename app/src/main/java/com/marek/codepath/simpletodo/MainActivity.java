a902ce2a687e5531633bc5f7ad5d92bd08e7e161
Marek <mbuchler@gmail.com>
2017-02-12 13:51:24 -0500

    after walking through tutorial

---
+++ app/src/main/java/com/marek/codepath/simpletodo/MainActivity.java
@@ -0,0 +1,76 @@
+package com.marek.codepath.simpletodo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static com.marek.codepath.simpletodo.R.id.etNewItem;
+
+public class MainActivity extends AppCompatActivity {
+    ArrayList<String> items;
+    ArrayAdapter<String> itemsAdapter;
+    ListView lvItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lvItems = (ListView)findViewById(R.id.lvItems);
+
+        readItems();
+        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        lvItems.setAdapter(itemsAdapter);
+
+        setupListViewListener();
+    }
+
+    private void readItems() {
+        File todoFile = new File(getFilesDir(), "todo.txt");
+        try {
+            items = new ArrayList<>(FileUtils.readLines(todoFile));
+        } catch(IOException ioe) {
+            items = new ArrayList<>();
+            ioe.printStackTrace();
+        }
+    }
+
+    private void writeItems() {
+        File todoFile = new File(getFilesDir(), "todo.txt");
+        try {
+            FileUtils.writeLines(todoFile, items);
+        } catch(IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    public void onAddItem(View v) {
+        EditText etNewItem = (EditText)findViewById(R.id.etNewItem);
+        String newItem = etNewItem.getText().toString();
+        itemsAdapter.add(newItem);
+        writeItems();
+        etNewItem.setText("");
+    }
+
+    private void setupListViewListener() {
+        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapter, View item, int pos, long id) {
+                items.remove(pos);
+                itemsAdapter.notifyDataSetChanged();
+                writeItems();
+                return true;
+            }
+        });
+    }
+}
